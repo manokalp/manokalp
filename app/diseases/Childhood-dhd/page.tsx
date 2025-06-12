f34f00e3e5d08602df885134a7b731936c9eabd5
DigitallyNext <digitallynext2017@gmail.com>
2025-06-12 19:16:27 +0530

    Comment out unused Link imports across multiple disease pages to streamline code and improve maintainability.

--- app/diseases/Childhood-dhd/page.tsx
+++ app/diseases/Childhood-dhd/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link"; 
 import { useState } from "react";
 // import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import playfair from "../../../fonts/playfair.module.css";
--- app/diseases/stress/page.tsx
+++ app/diseases/stress/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 import { useState } from "react";
 import playfair from "../../../fonts/playfair.module.css";
 
